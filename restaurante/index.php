dcc88072b46209cc8bd50b6db010938af6ad9a0f
SoftwaRealles <softwarealles@outlook.com>
2019-05-30 13:22:54 -0300

    Projeto Restaurante v0

---
+++ restaurante/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet"/>
+        <meta name="viewport" content="device-width, initial-scale=1"/>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <meta charset="utf-8">
+        <title>Restaurante | Home</title>
+    </head>
+    <body>
+        <!-- Start header /-->
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navbar navbar-expand-md navbar-light bg-light d-flex">
+                            <a class="navbar-brand" href="#">Restaurante</a>
+                            <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse justify-content-end" id="nav-target">
+                                <ul class="navbar-nav text-right">
+                                    <li class="nav-item active"><a class="nav-link">Home</a></li>
+                                    <li class="nav-item"><a class="nav-link">Cardápio</a></li>
+                                    <li class="nav-item"><a class="nav-link">Promoções</a></li>
+                                    <li class="nav-item"><a class="nav-link">Contato</a></li>
+                                    <li class="nav-item"><a class="nav-link">Sobre</a></li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+                                            Social
+                                        </a>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">
+                                                Facebook
+                                            </a>
+                                            <a class="dropdown-item" href="#">
+                                                Twitter
+                                            </a>
+                                            <a class="dropdown-item" href="#">
+                                                Instagram
+                                            </a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- End header /-->
+        
+        <!-- Start hero /-->
+        <section class="hero">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="text-hero">
+                            <h1>Titulo Restaurante</h1>
+                            <p>subtitulo Restaurnate</p>
+                            <button class="btn btn-danger" type="button">ver mais</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End hero /-->
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
