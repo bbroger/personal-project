e2e7ac5146ed33a698c6d929acf98a20e1c64bb0
SoftwaRealles <softwarealles@outlook.com>
2019-05-30 14:47:20 -0300

    Projeto Restaurante (correções) V0.1

--- restaurante/index.php
+++ restaurante/index.php
@@ -51,15 +51,15 @@
             </div>
         </header>
         <!-- End header /-->
-        
+
         <!-- Start hero /-->
         <section class="hero">
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="text-hero">
-                            <h1>Titulo Restaurante</h1>
-                            <p>subtitulo Restaurnate</p>
+                            <h1>Título Restaurante</h1>
+                            <p>subtitulo Restaurante</p>
                             <button class="btn btn-danger" type="button">ver mais</button>
                         </div>
                     </div>
@@ -67,6 +67,293 @@
             </div>
         </section>
         <!-- End hero /-->
+
+        <!-- Start dish-day (daily special)/-->
+        <div class="container">
+            <section class="dish-day effect2">
+                <h2 class="text-center">Pratos feitos</h2>
+                <div class="row">
+                    <div class="col-md-12"></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img class="thumb-dish" src="img/thumb01.jpg" alt="Prato feito 01"/>
+                        <p class="text-center">Prato feito</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img class="thumb-dish" src="img/thumb02.jpg" alt="Prato feito 02"/>
+                        <p class="text-center">Prato feito</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img class="thumb-dish" src="img/thumb03.jpg" alt="Prato feito 03"/>
+                        <p class="text-center">Prato feito</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img class="thumb-dish" src="img/thumb04.jpg" alt="Prato feito 04"/>
+                        <p class="text-center">Prato feito</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- End dish-day (daily special)/-->
+
+        <!-- Start portion (serving)/-->
+        <div class="container">
+            <section class="dish-day my-4">
+                <h2 class="text-center">Porções</h2>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumb05.jpg"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Batata frita</h5>
+                                <p class="card-text">batata frita, sal a gosto, molho (mostarda, ketchup).</p>
+                            </div>
+                            <a class="btn btn-primary">Pedir</a>
+                            <div class="card-footer">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumb06.jpg"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Mandioca frita</h5>
+                                <p class="card-text">mandioca frita, molho especial (mostarda, ketchup, molho de alho)</p>
+                            </div>
+                            <a class="btn btn-primary">Pedir</a>
+                            <div class="card-footer">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumb07.jpg"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Linguiça acebolada</h5>
+                                <p class="card-text">linguiça frita com cebola, molho (mostarda, katchup)</p>
+                            </div>
+                            <a class="btn btn-primary">Pedir</a>
+                            <div class="card-footer">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card">
+                            <img class="card-img-top" src="img/thumb08.jpg"/>
+                            <div class="card-body">
+                                <h5 class="card-title">Camarão</h5>
+                                <p class="card-text">camarão, oregano, sal, molho (mostarda, katchup)</p>
+                            </div>
+                            <a class="btn btn-primary">Pedir</a>
+                            <div class="card-footer">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- End portion (serving)/-->
+
+        <!-- End menu/-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="cardapio my-4">
+                        <h1 class="text-center">Menu</h1>
+                        <nav>
+                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                <a class="nav-item nav-link active" id="nav-lanches-tab" data-toggle="tab" href="#tab-lanches" role="tab">Lanches</a>
+                                <a class="nav-item nav-link" id="nav-bebidas-tab" data-toggle="tab" href="#tab-bebidas" role="tab">Prato do dia</a>
+                                <a class="nav-item nav-link" id="nav-salgados-tab" data-toggle="tab" href="#tab-salgados" role="tab">Bebidas</a>
+                            </div>
+                        </nav>
+                        <div class="tab-content" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="tab-lanches" role="tabpanel">
+                                <div class="row">
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Frango a passarinho</h2>
+                                        <p>frango em pedaços, alhos triturado, colorau, salsa, suco de limão, óleo, alho fatiado.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Isca de frango na cerveja</h2>
+                                        <p>peito de frango em cubos, alho amassado, sal, pimenta, cerveja, bacon em cubos.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Dois queijos à milanesa</h2>
+                                        <p>queijo provolone em cubos, queijo coalho em cubos, claras, orégano.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Bolinho de bacalhau</h2>
+                                        <p>bacalhau, azeite, cebola picada, alho, pimenta, Orégano, batata.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Salgado</h2>
+                                        <p>esfiha de carne, sasicha enrolada, kibe, bauru, esfiha de frango.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade show" id="tab-bebidas" role="tabpanel">
+                                <div class="row">
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Segunda-feira: Virado à paulista</h2>
+                                        <p>Posuere ultricies hac ultricies nisi per leo lobortis ornare venenatis morbi.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Terça-feira: Bife a rolê</h2>
+                                        <p>Posuere ultricies hac ultricies nisi per leo lobortis ornare venenatis morbi.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Quarta-feira: Feijoada</h2>
+                                        <p>Posuere ultricies hac ultricies nisi per leo lobortis ornare venenatis morbi.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Quinta-feira: Macarrão</h2>
+                                        <p>Posuere ultricies hac ultricies nisi per leo lobortis ornare venenatis morbi.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Sexta-feira: Peixe frito</h2>
+                                        <p>Posuere ultricies hac ultricies nisi per leo lobortis ornare venenatis morbi.</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade show" id="tab-salgados" role="tabpanel">
+                                <div class="row">
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Refrigerantes</h2>
+                                        <p>Coca-Cola, Pepsi, Kuat, Fanta, Tubaina...</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Sucos naturais</h2>
+                                        <p>morango, abacaxi, laranja, acerola...</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Cervejas</h2>
+                                        <p>Bavaria, Devassa, Brahma, Itaipava...</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Coquetéis</h2>
+                                        <p>Martini clássico, Vodka Martini, Martini dirty, Martini de maça...</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                    <div class="col-md-8 cardapio-lanche">
+                                        <h2>Vinhos</h2>
+                                        <p>espumantes frânces, vinho rosé espanhol, vinho branco italiano...</p>
+                                    </div>
+                                    <div class="col-md-4 preco">
+                                        R$ 10,00                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End menu/-->
+
+        <!-- Start funcionamento (operation)/-->
+        <section class="funcionamento">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul>
+                            <h4>Horário de funcionamento</h4>
+                            <li>Segunda a quinta-feira – 11h30 as 15h | 17h30 as 23h</li>
+                            <li>Sexta-feira – 11h30 as 23h</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End funcionamento (operation)/-->
+
+        <!-- End footer/-->
+        <footer class="page-footer font-small cyan darken-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 pt-5">
+                        <div class="mb-3 text-center">
+                            <a class="fb-ic" href="#">
+                                <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                            </a>
+                            <a class="tw-ic" href="#">
+                                <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                            </a>
+                            <a class="ins-ic" href="#">
+                                <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                            </a>
+                            <a class="pin-ic" href="#">
+                                <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+                            </a>
+                            <p class="mt-5">Rua loren ipsum, 814 - SP</p>
+                            <p>(11) 0000-0000</p>
+                            <p>contato@restaurante.com.br</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright text-center py-3">© 2019 Copyright: <span class="stylefooter">Restaurante</span> | todos os direitos reservados.
+            </div>
+        </footer>
+        <!-- End footer/-->
+
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
