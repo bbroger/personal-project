440a130d4c304abeb46231a1cb9a4c2315f082fa
SoftwaRealles <softwarealles@outlook.com>
2019-05-30 15:15:54 -0300

    Projeto Joas Pinturas V0

---
+++ joaspinturas/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="keywords" content="Joas Pinturas, Pintor Profissional, pintor qualificado em pintura residencial, comercial e mais. Orçamento grátis. Contatos: (11) 94847-1604, joas pinturas, pintor profissional, joaspinturas,">
+        <meta name="robots" content="index, follow"/>
+        <meta name="autor" content="SoftwaRealles"/>
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Joas Pinturas | Pintor Profissional" />
+        <meta property="og:description" content="Joas Pinturas | Pintor Profissional." />
+        <meta property="og:url" content="https://joaspinturas.tk/" />
+        <meta property="og:site_name" content="Joas Pinturas | Pintor Profissional." />
+        <meta property="og:image" content="https://joaspinturas.tk/img/icon.png" />
+        <meta property="og:image" content="https://joaspinturas.tk/img/icon.jpg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="canonical" href="https://joaspinturas.tk" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://www.joaspinturas.tk/css/style.css"/>
+        <link rel="icon" href="img/icon.png"/>
+        <link rel="icon" href="img/icon.jpg"/>
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+        <title>Joas Pinturas | HOME</title>
+    </head>
+    <body>
+        
+        <!-- INICIO MENU RESPONSIVO -->
+        <header>
+            <nav class="navbar navbar-expand-md navbar-light bg-menu">
+                <div class="container">
+                    <a class="navbar-brand logo" href="#">
+                        Joas Pinturas
+                    </a>
+                    <button class="navbar-toggler" data-toggle="collapse" data-target="#menu-principal">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="menu-principal">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="navbar-item">
+                                <a class="nav-link">home</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a class="nav-link">serviços</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a class="nav-link">clientes</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a class="nav-link">sobre</a>
+                            </li>
+                            <li class="navbar-item">
+                                <a class="nav-link">contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <!-- FIM MENU RESPONSIVO /-->
+
+        <!-- INICIO HOME -->
+        <section class="home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="https://www.joaspinturas.tk/img/home.jpg"/> 
+                    </div>
+                    <div class="col-md-6 d-flex">
+                        <div class="align-self-center">
+                            <h1 class="titulo">Pintor profissional</h1>
+                            <p> Com mais de 30 anos de experiencia, qualificado a
+                                atuar em diversos projetos complexos.<br>
+                                Pinturas de lojas, residências, apartamentos, o mínimo de incômodo.</p>
+                            <a class="btn btn-info" href="#">orçamento</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- FIM HOME /-->
+
+        <!-- INICIO SERVIÇOS -->
+        <section class="servicos">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!--<div class="justify-content-center bg-dark">-->
+                        <div class="text-center my-4">
+                            <h2>serviços</h2>
+                            <p>
+                                Precisa fazer acabamento, reformar ou decorar?<br>
+                                com Joas Pinturas | Pintor Profissional fica mais
+                                fácil, rápido e prático.
+                            </p>
+                        </div>
+                        <div class="row text-center justify-content-center py-4">
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="https://www.joaspinturas.tk/img/pintura.jpg"/>
+                                <h4 class="mt-2">pinturas</h4>
+                                <p>
+                                    Vai além do básico e permite que você
+                                    transforme seu ambiente, essenciais para sua vida.
+                                    Simples como tem que ser.
+                                </p>
+                            </div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="https://www.joaspinturas.tk/img/grafiato.jpg"/>
+                                <h4 class="mt-2">grafiatos</h4>
+                                <p>
+                                    Decore sua parede com mais diverssos tipos
+                                    e modelos.
+                                </p>                      
+                            </div>
+                            <div class="col-md-4">
+                                <img class="img-fluid" src="https://www.joaspinturas.tk/img/textura.jpg"/>
+                                <h4 class="mt-2">texturas</h4>
+                                <p>
+                                    Deixa sua parede mais bonita e atraente,
+                                    mais variada cores e modelos.
+                                </p>                     
+                            </div>
+                            <a class="btn btn-info" href="#">saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- FIM SERVIÇOS /-->
+
+        <section class="text-center justify-content-center ">
+            <h2>clientes</h2>
+            <div class="container my-4">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="https://www.joaspinturas.tk/img/thumb-grande1.jpg"/>
+                    </div>
+                    <div class="col-md-6 d-none d-md-block">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img class="img-fluid" src="https://www.joaspinturas.tk/img/thumb-pequena1.jpg"/>
+                            </div>
+                            <div class="col-md-6">
+                                <img class="img-fluid" src="https://www.joaspinturas.tk/img/thumb-pequena2.jpg"/>
+                            </div>                            
+                        </div>
+                        <div class="row">
+                            <div class="mt-4 col-md-6">
+                                <img class="img-fluid" src="https://www.joaspinturas.tk/img/thumb-pequena3.jpg"/>
+                            </div>
+                            <div class="col-md-6">
+                                <img class="mt-4 img-fluid" src="https://www.joaspinturas.tk/img/thumb-pequena4.jpg"/>
+                            </div>                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="btn btn-info" href="#">veja mais</a>
+        </section>
+        
+        <!-- INICIO FOOTER -->
+        <footer class="mt-4">
+            <div class="container">
+                <div class="row">
+                    <div class="endereco col-md-4">
+                        <p>Multitintas Jardins</p>
+                        <p>Alameda Lorena, 814 - Jardins</p>
+                        <p>São Paulo - SP</p>
+                        <p>joaspinturas@outlook.com</p>
+                        <p>(11) 9 4847-1604</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2 class="logofooter">joas pinturas</h2>
+                        <p class="menu-footer">
+                            <a class="" href="#">serviços</a>
+                            <a class="" href="#">clientes</a>
+                            <a class="" href="#">sobre</a>
+                            <a class="" href="#">contato</a>
+                        </p>
+                        <p>
+                            joas pinturas &copy; 2019 | todos os direitos reservados.<br>
+                            desenvolvido por SoftwaRealles.com
+                        </p>
+                    </div>
+                    <div class="redes col-md-4 justify-content-center">
+                        <a class="" href="https://www.facebook.com/JoasRealles" target="_black"><i class="fab fa-facebook-square"></i></a>
+                        <a class="" href="#"><i class="fab fa-twitter-square ml-2"></i></a>
+                        <a class="" href="#"><i class="fab fa-instagram ml-2"></i></a>
+                        <a class="" href="#"><i class="fab fa-youtube ml-2"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+        <!-- JQUERY JS /-->
+        <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+</html>
